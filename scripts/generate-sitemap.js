1f6305dfea7331021c8ec438321777886bddd6fa
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2026-06-20 08:10:47 +0100

    Some cleanup

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
-const { bundleMDX } = require('mdx-bundler');
 
 function addPage(page) {
   const path = page
     .replace('src/pages', '')
     .replace('.page.js', '')
-    .replace('.page.mdx', '')
     .replace('/index', '/');
   const route = path === '/index' ? '' : path;
 
@@ -18,34 +16,17 @@ function addPage(page) {
   </url>`;
 }
 
-async function addPost(post) {
-  const source = fs.readFileSync(post, 'utf-8');
-  const { frontmatter } = await bundleMDX({ source });
-
-  if (process.env.NODE_ENV === 'production' && frontmatter.draft) return;
-
-  const path = post.replace('src/posts', '/articles').replace('.mdx', '');
-
-  return `  <url>
-    <loc>${`${process.env.NEXT_PUBLIC_WEBSITE_URL}${path}`}</loc>
-    <changefreq>monthly</changefreq>
-  </url>`;
-}
-
 async function generateSitemap() {
   const { globby } = await import('globby');
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
-    'src/pages/**/*{.page.js,.page.mdx}',
+    'src/pages/**/*.page.js',
     '!src/pages/_*.js',
     '!src/pages/api',
   ]);
-  const postUrls = await globby(['src/posts/**/*.mdx']);
-  const posts = await Promise.all(postUrls.map(addPost));
 
   const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(addPage).filter(Boolean).join('\n')}
-${posts.filter(Boolean).join('\n')}
 </urlset>\n`;
 
   fs.writeFileSync('public/sitemap.xml', sitemap);
